83ea2ed711918668f6c41f7abb561cdb4dc3f7cf
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-07-24 17:59:50 -0400

    navbar started

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import "./navbar.css"
 function Navbar (){
     return(
         <header className="navbar">
-            <img className="splashLogo" src="./TheoZonLogo.png" alt="TheoZon"></img>
+            <img className="splashLogo" src="./TheoZonLogo1.png" alt="TheoZon"></img>
         </header>
     )
 }
